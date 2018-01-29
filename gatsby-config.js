a622997c46d96dd02460cd4a634fc0210e5acd86
Paul Grock <paulgrock@gmail.com>
2018-01-28 18:04:00 -0800

    Update siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'paulgrock.com',
     author: 'Paul Grock',
-    siteUrl: 'paulgrock.com',
+    siteUrl: 'https://competent-pike-550e2c.netlify.com',
     description: `Full stack engineer. HTML, CSS and JavaScript on the client and server are usually how I roll. I'm also into games and basketball.`
   },
   plugins: [
